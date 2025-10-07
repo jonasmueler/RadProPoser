88821dabf074e6c2aaac082242887b31fd47e3d5
Jonas Mueller <jonas.leo.mueller@fau.de>
2025-10-07 09:54:39 +0200

    cleanup

--- tools/config.py
+++ tools/config.py
@@ -2,87 +2,108 @@ import os
 
 ## globals
 SEQLEN = 8
-MODELNAME = None
+MODELNAME = None # set the name of the model you want to train  here
 
 ############################################### HPE #######################################################################
 ## model paramas
-#RadProPoserVAE
-# define hyperparameters
 
-
-# 20, 5 gaussian, 1,1 laplace, laplace gaussian 1, 5
-TRAINCONFIG = {"learningRate": 0.001, 
-          "weightDecay": 0.0001,
-          "epochs": 24, 
-          "batchSize": 16, 
-         "optimizer": "adam", 
-          "device": "cuda", 
-          "betas": (0.9, 0.999), # momentum and scaling for ADAM, 
-          "lrDecay": 0.99, 
-         "beta": 20, #20
-         "gamma": 5, # 5 
-          "nll": True,
-        "evd": False,
-          "nf": False
-          }
-
-          
-"""
-#RadProPoserEvidential
-# define hyperparameters
-TRAINCONFIG = {"learningRate": 0.0001, 
-          "weightDecay": 0.0001,
-          "epochs": 24, 
-          "batchSize": 16, 
+if MODELNAME in ("RPPgaussianGaussian", "RPPgaussianGaussianCov"):
+  # 20, 5 gaussian, 1,1 laplace, laplace gaussian 1, 5
+  TRAINCONFIG = {"learningRate": 0.001, 
+            "weightDecay": 0.0001,
+            "epochs": 24, 
+            "batchSize": 16, 
           "optimizer": "adam", 
-          "device": "cuda", 
-          "betas": (0.9, 0.999), # momentum and scaling for ADAM, 
-          "lrDecay": 0.99, 
-          "lambda":0.001, # 0.001
-          "nll": False,
-          "evd": True,
-          "nf": False
-          }
-"""
-"""
-# radproposer noermalizing flow 
-TRAINCONFIG = {"learningRate": 0.0001, 
-          "weightDecay": 0.0001,
-          "epochs": 24, 
-          "batchSize": 32, 
-          "optimizer": "adam", 
-          "device": "cuda", 
-          "betas": (0.9, 0.999), # momentum and scaling for ADAM, 
-          "lrDecay": 0.99, 
-          "gamma": 1,
-          "beta": 1,
-          "nll": False,
-          "evd": False, 
-          "nf": True
-          }
-"""
-
-
-
-
-## Ho et al
-# define hyperparameters
-
-"""
-TRAINCONFIG = {"learningRate": 0.0001, 
-          "weightDecay": 0.0001,
-          "epochs": 40, 
-          "batchSize": 16, #16 #64 
-          "optimizer": "adam", 
-          "device": "cuda", 
-          "betas": (0.9, 0.999), # momentum and scaling for ADAM, 
-          "lrDecay": 0.99, 
-          "nll": False,
+            "device": "cuda", 
+            "betas": (0.9, 0.999), # momentum and scaling for ADAM, 
+            "lrDecay": 0.99, 
+          "beta": 20, 
+          "gamma": 5,  
+            "nll": True,
           "evd": False,
-          "nf": False, 
-          "HR_SINGLE": True
-          }
-"""
+            "nf": False
+            }
+
+if MODELNAME in ("RPPlaplaceLaplace"):
+  TRAINCONFIG = {"learningRate": 0.001, 
+            "weightDecay": 0.0001,
+            "epochs": 24, 
+            "batchSize": 16, 
+          "optimizer": "adam", 
+            "device": "cuda", 
+            "betas": (0.9, 0.999), # momentum and scaling for ADAM, 
+            "lrDecay": 0.99, 
+          "beta": 1, 
+          "gamma": 1, 
+            "nll": True,
+          "evd": False,
+            "nf": False
+            }
+  
+if MODELNAME in ("RPPlaplaceGaussian"):
+  TRAINCONFIG = {"learningRate": 0.001, 
+            "weightDecay": 0.0001,
+            "epochs": 24, 
+            "batchSize": 16, 
+          "optimizer": "adam", 
+            "device": "cuda", 
+            "betas": (0.9, 0.999), # momentum and scaling for ADAM, 
+            "lrDecay": 0.99, 
+          "beta": 1, 
+          "gamma": 5, 
+            "nll": True,
+          "evd": False,
+            "nf": False
+            }
+
+        
+if MODELNAME in ("RPPevidential"):
+  TRAINCONFIG = {"learningRate": 0.0001, 
+            "weightDecay": 0.0001,
+            "epochs": 24, 
+            "batchSize": 16, 
+            "optimizer": "adam", 
+            "device": "cuda", 
+            "betas": (0.9, 0.999), # momentum and scaling for ADAM, 
+            "lrDecay": 0.99, 
+            "lambda":0.001, # 0.001
+            "nll": False,
+            "evd": True,
+            "nf": False
+            }
+
+
+if MODELNAME in ("RPPnormalizingFlow"):
+  TRAINCONFIG = {"learningRate": 0.0001, 
+            "weightDecay": 0.0001,
+            "epochs": 24, 
+            "batchSize": 32, 
+            "optimizer": "adam", 
+            "device": "cuda", 
+            "betas": (0.9, 0.999), # momentum and scaling for ADAM, 
+            "lrDecay": 0.99, 
+            "gamma": 1,
+            "beta": 1,
+            "nll": False,
+            "evd": False, 
+            "nf": True
+            }
+
+if MODELNAME in ("HoEtAlBaseline"):
+  TRAINCONFIG = {"learningRate": 0.0001, 
+            "weightDecay": 0.0001,
+            "epochs": 40, 
+            "batchSize": 16, #16 #64 
+            "optimizer": "adam", 
+            "device": "cuda", 
+            "betas": (0.9, 0.999), # momentum and scaling for ADAM, 
+            "lrDecay": 0.99, 
+            "nll": False,
+            "evd": False,
+            "nf": False, 
+            "HR_SINGLE": True
+            }
+
 
 
 # paths
